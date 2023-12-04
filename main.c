e839870b8f7fd02ae1cb2d3ee8c945320a3e4aba
Wongani <wonganichulu@gmail.com>
2023-12-04 15:17:17 +0200

    Added test file main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+	char *string = "my name is Stone Cold";
+	_printf("Hello world! %s and I'm a 100%% badass\n", string);
+	_printf("My highest academic grade was %d%% and I am  %i year young\n", 98.76, 31);
+	_printf("");
+	return (0);
+}
